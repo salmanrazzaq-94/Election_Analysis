7fe9ef475ef159de5438a99a378dad545bf9f48c
Salman Razzaq <muhammad.razzaq2@studio.unibo.it>
2022-08-22 12:00:19 -0400

    analysis added

--- Pypoll.py
+++ Pypoll.py
@@ -1,74 +1,94 @@
-# Add our dependencies.
+# Needed Data
+#1. Total number of votes cast
+#2. Complete list of candidates who received votes
+#3. Total number of votes each candidate received
+# 4. Percentage of votes each candidate received
+# 5. Winner of the election based on popular
+
+# Add dependencies
 import csv
 import os
-# Assign a variable to load a file from a path.
+
+# Assign a vaiable for the file to load and the path
 file_to_load = os.path.join("Resources", "election_results.csv")
-# Assign a variable to save the file to a path.
+# Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Initialize a total vote counter.
+
+# 1. Initialize a total vote counter
 total_votes = 0
-# Candidate options and candidate votes.
+
+# Candidate options and votes
 candidate_options = []
 candidate_votes = {}
-# Track the winning candidate, vote count, and percentage.
+
+# Winning Candidate and Winning Count Tracker
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
-# Open the election results and read the file.
+
+# Open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-    # Read the header row.
+
+    # Read the header row
     headers = next(file_reader)
-    # Print each row in the CSV file.
+    
+    # Print each row in the CSV file
     for row in file_reader:
-        # Add to the total vote count.
+        #2. Add to the total vote count
         total_votes += 1
-        # Get the candidate name from each row.
+
+        # If the candidate doesn't match an existing candidate...
         candidate_name = row[2]
-        # If the candidate does not match any existing candidate, add the
-        # the candidate list.
         if candidate_name not in candidate_options:
-            # Add the candidate name to the candidate list.
+            # Add it to the list of candidates
             candidate_options.append(candidate_name)
-            # And begin tracking that candidate's voter count.
+            # Begin tracking candidate's vote count
             candidate_votes[candidate_name] = 0
-        # Add a vote to that candidate's count.
+        # Increment vote count
         candidate_votes[candidate_name] += 1
 
-# Save the results to our text file.
+# Using the open() function with the "W" mode we will write data to the file
 with open(file_to_save, "w") as txt_file:
-    # After opening the file print the final vote count to the terminal.
+# Printthe final vote count to the terminal
     election_results = (
         f"\nElection Results\n"
-        f"-------------------------\n"
+        f"--------------------------------------\n"
         f"Total Votes: {total_votes:,}\n"
-        f"-------------------------\n")
+        f"---------------------------------------\n")
     print(election_results, end="")
-    # After printing the final vote count to the terminal save the final vote count to the text file.
+    # Save the final vote count to the text file
     txt_file.write(election_results)
-    for candidate_name in candidate_votes:
-        # Retrieve vote count and percentage.
-        votes = candidate_votes[candidate_name]
-        vote_percentage = float(votes) / float(total_votes) * 100
-        candidate_results = (
-            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-        # Print each candidate's voter count and percentage to the terminal.
+    # Determine percentage of vote for each candidate
+    #1. Iterate through candidate list
+    for candidate_name in candidate_votes:
+        #2. Retrieve vote count
+        votes = candidate_votes[candidate_name]
+        #3. Calculate the percentage of votes
+        vote_percentage = float(votes) / float(total_votes) * 100
+    
+        #4. Print the candidate and percentage of votes
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
         print(candidate_results)
-        #  Save the candidate results to our text file.
         txt_file.write(candidate_results)
-        # Determine winning vote count, winning percentage, and winning candidate.
+
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
-            winning_candidate = candidate_name
             winning_percentage = vote_percentage
-    # Print the winning candidate's results to the terminal.
+            winning_candidate = candidate_name
+
+    # Print winning candidate's results to the terminal
     winning_candidate_summary = (
-        f"-------------------------\n"
+        f"------------------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"-------------------------\n")
+        f"------------------------------\n")
     print(winning_candidate_summary)
-    # Save the winning candidate's results to the text file.
+    # Save winning candidate's results to the text file
     txt_file.write(winning_candidate_summary)
+
+    
--- README.md
+++ README.md
@@ -1,15 +1,69 @@
-Election_Analysis
-=================
+# Election_Analysis
 
-In this project, our final Python script was able to deliver the following
-information when the script is run: 
+## Project Overview
+A Colorado Board of Elections employee has given you the following tasks to complete the election audit of a recent local congressional election.
 
--   Total number of votes cast
+1. Calculate the total number of votes cast.
+2. Get a complete list of candidates who received votes.
+3. Calculate the total number of votes each candidate received.
+4. Calculate the percentage of votes each candidate won.
+5. Determine the winner of the election based on popular vote.
 
--   A complete list of candidates who received votes
+## Resources
+- Data Source: election_results.csv
+- Software: Python 3.7.6, Visual Studio Code, 1.67.1
 
--   Total number of votes each candidate received
+## Summary
+The analysis of the election shows that:
+- There were 369,711 votes cast in the election.
+- The candidates were:
+   - Charles Casper Stockham
+   - Diana DeGette
+   - Raymon Anthony Doane
+- The candidate results were:
+   - Charles Casper Stockham received 23.0% of the vote and 85,213 votes.
+   - Diana DeGette received 73.8% of the vote and 272,892 votes.
+   - Raymon Anthony Doane received 3.1% of the vote and 11,606 votes.
 
--   Percentage of votes each candidate won
+The winner of the election was:
+  - Diana DeGette, who received 73.8% of the vote with 272,892 votes.
 
--   The winner of the election based on popular vote
+## Challenge Overview
+The Colorado Board of Elections employee has requested additional information for the election audit of the congressional election.
+
+1. The voter turnout for each county.
+2. The percentage of votes from each county out of the total votes.
+3. The county with the highest turnout.
+
+## Challenge Summary
+### Election audit results
+* A total of 369,711 votes were cast in the election.
+* Votes by county:
+  
+   ![county_votes](Resources/county_votes.png)
+  
+* The largest voter turnout was in Denver County
+* Candidate total votes and percentages:
+
+   ![candidate_votes](Resources/candidate_votes.png)
+
+* The winning candidate was:
+
+   ![candidate_votes](Resources/Winner.png)
+
+### Election audit summary
+The attached script can easily be modified for use in future elections. Sample modifications include:
+* For city elections the following sections can easily be modified by replacing county with city:
+   ![city1](Resources/city1.png)
+   
+   ![city2](Resources/city2.png)
+   
+   ![city3](Resources/city3.png)
+   
+   ![city4](Resources/city4.png)
+   
+   ![city5](Resources/city5.png)
+   
+   ![city6](Resources/city6.png)
+   
+Please let us know if you need any additional changes and we look forward to working with you on future elections.
